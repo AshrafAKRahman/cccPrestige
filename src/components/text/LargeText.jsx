1f0549f39a4d209fc36afa7060e4905856923fab
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-06-21 14:37:37 +0100

    fixed the largeText component bug to allow text to span across the screen

--- src/components/text/LargeText.jsx
+++ src/components/text/LargeText.jsx
@@ -1,7 +1,7 @@
 const LargeText = ({ titleClassName, title }) => {
   return (
     <div className="relative">
-      <div className="absolute inset-0 flex items-center justify-center">
+      <div className="">
         <h1 className={`text-white ${titleClassName}`}>{title}</h1>
       </div>
     </div>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,88 +1,3 @@
-// // import React, { useEffect, useRef } from "react";
-// import { gsap } from "gsap";
-// import Banner from "@/components/banner/Banner";
-// import LargeButton from "@/components/buttons/LargeButton";
-// import LargeText from "@/components/text/LargeText";
-// import React, { useEffect, useRef } from "react";
-
-// const LandingPage = (props) => {
-//   const boxA = useRef(null);
-//   const boxB = useRef(null);
-//   const boxC = useRef(null);
-
-//   const handleScroll = (event) => {
-//     const deltaY = event.deltaY;
-
-//     if (deltaY > 0) {
-//       // Scrolling down
-//       if (boxA.current.style.opacity === "0") {
-//         gsap.to(boxA.current, { opacity: 1 });
-//         gsap.to(boxB.current, { opacity: 0 });
-//         gsap.to(boxC.current, { opacity: 0 });
-//       } else if (boxB.current.style.opacity === "0") {
-//         gsap.to(boxB.current, { opacity: 1 });
-//         gsap.to(boxA.current, { opacity: 0 });
-//         gsap.to(boxC.current, { opacity: 0 });
-//       } else if (boxC.current.style.opacity === "0") {
-//         gsap.to(boxC.current, { opacity: 1 });
-//         gsap.to(boxA.current, { opacity: 0 });
-//         gsap.to(boxB.current, { opacity: 0 });
-//       }
-//     } else if (deltaY < 0) {
-//       // Scrolling up
-//       if (boxC.current.style.opacity === "1") {
-//         gsap.to(boxC.current, { opacity: 0 });
-//         gsap.to(boxB.current, { opacity: 1 });
-//       } else if (boxB.current.style.opacity === "1") {
-//         gsap.to(boxB.current, { opacity: 0 });
-//         gsap.to(boxA.current, { opacity: 1 });
-//       } else if (boxA.current.style.opacity === "1") {
-//         gsap.to(boxA.current, { opacity: 0 });
-//       }
-//     }
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("wheel", handleScroll);
-
-//     return () => {
-//       window.removeEventListener("wheel", handleScroll);
-//     };
-//   }, []);
-
-//   return (
-//     <div>
-//       <Banner />
-//       <div ref={boxA} className="boxA" style={{ opacity: 0 }}>
-//         <LargeText
-//           titleClassName="text-5xl text-bold text-white"
-//           title="Hello World !"
-//         />
-//       </div>
-//       <div ref={boxB} className="boxB" style={{ opacity: 0 }}>
-//         <LargeText
-//           titleClassName="text-5xl text-bold text-white"
-//           title="Hello you !"
-//         />
-//       </div>
-//       <div ref={boxC} className="boxC" style={{ opacity: 0 }}>
-//         <LargeText
-//           titleClassName="text-5xl text-bold text-white"
-//           title="Hello C!"
-//         />
-//       </div>
-//       <div className="absolute mt-20 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
-//         <LargeButton
-//           text="Book now"
-//           href="/Properties"
-//           className="bg-blue-200 shadow-xl shadow-white/30"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LandingPage;
 import React, { useEffect, useRef } from "react";
 import { gsap, Power2 } from "gsap";
 import Banner from "@/components/banner/Banner";
@@ -152,37 +67,40 @@ const LandingPage = () => {
   }, []);
 
   return (
-    <div>
+    <div className="relative">
       <Banner />
-      <div
-        ref={(el) => (boxes.current[0] = el)}
-        className="boxA"
-        style={{ opacity: 1, visibility: "visible" }}
-      >
-        <LargeText
-          titleClassName="text-5xl text-bold text-white"
-          title="Hello World !"
-        />
-      </div>
-      <div
-        ref={(el) => (boxes.current[1] = el)}
-        className="boxB"
-        style={{ opacity: 0, visibility: "hidden" }}
-      >
-        <LargeText
-          titleClassName="text-5xl text-bold text-white"
-          title="Hello you !"
-        />
-      </div>
-      <div
-        ref={(el) => (boxes.current[2] = el)}
-        className="boxC"
-        style={{ opacity: 0, visibility: "hidden" }}
-      >
-        <LargeText
-          titleClassName="text-5xl text-bold text-white"
-          title="Hello C!"
-        />
+
+      <div className="absolute pb-10">
+        <div
+          ref={(el) => (boxes.current[0] = el)}
+          className="boxA"
+          style={{ opacity: 0 }}
+        >
+          <LargeText
+            titleClassName="text-5xl text-bold text-white w-full"
+            title="Welcome to Londons leading luxury lifestyle website"
+          />
+        </div>
+        <div
+          ref={(el) => (boxes.current[1] = el)}
+          className="boxB"
+          style={{ opacity: 0 }}
+        >
+          <LargeText
+            titleClassName="text-5xl text-bold text-white"
+            title="With over 15 years of experience in the industry we can cater for all your accomodation and transportation needs and wants"
+          />
+        </div>
+        <div
+          ref={(el) => (boxes.current[2] = el)}
+          className="boxC"
+          style={{ opacity: 0 }}
+        >
+          <LargeText
+            titleClassName="text-5xl text-bold text-white"
+            title="Choose your vehicle and propoerty as well as any other concierge services in one place"
+          />
+        </div>
       </div>
       <div className="absolute mt-20 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
         <LargeButton
